63682ffd8c8e0fc1fdcdaa842093aa144c6a2bdd
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-06-27 20:35:08 -0300

    feat: desafio 005 sql order by limit

---
+++ Desafio_005_SQL_order_limit/app.js
@@ -0,0 +1,27 @@
+const Database = require(`better-sqlite3`)
+const db = new Database(`../banco.db`)
+
+db.exec(`
+    DROP TABLE IF EXISTS usuarios;
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        email TEXT NOT NULL
+    )    
+`)
+
+const inserir = db.prepare(`INSERT INTO usuarios (nome,email) VALUES (?,?)`)
+
+inserir.run(`Luiz`,`luiz@gmail.com`)
+inserir.run(`Stefani`,`stefani@gmail.com`)
+inserir.run(`Gregory`,`gregory@gmail.com`)
+inserir.run(`Maite`,`maite@gmail.com`)
+inserir.run(`Celia`,`celia@gmail.com`)
+
+const usuario = db.prepare(`SELECT * FROM usuarios ORDER BY nome ASC LIMIT 2`).all()
+
+console.log(usuario)
+
+const usuarioTest = db.prepare(`SELECT * FROM usuarios ORDER BY nome DESC`).all()
+
+console.log(usuarioTest)
